47a08d373df42b8d37b462ec90b91f4836dcb671
Arun kumar D <arunmass0273@gmail.com>
2024-08-31 03:00:50 +0530

    search page listing done

--- src/app/search/page.jsx
+++ src/app/search/page.jsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Error from "@/components/Error";
+import Header from "@/components/Header";
 import Loading from "@/components/Loading";
 import {
   exportTrackingData,
@@ -47,12 +48,26 @@ export default function SearchPage() {
 
   return (
     <>
-      <div>Header</div>
-      <div>Chatbot</div>
+      <Header />
+      <div>
+        {data.results.map((item) => {
+          return <a href={`/item/${item.pid}`}>
+            <div className="flex py-6 w-[60%] px-2 shadow-inner hover:shadow-slate-400 mb-5 hover:bg-slate-100  rounded-lg ml-10">
+              <div>
+                <img src={item.images[0]} alt={item.title} className="w-[180px] h-[100] py-5 px-6 hover:scale-110 duration-150 cursor-pointer rounded-sm" />
+              </div>
+              <div className="px-20  ">
+                <h2 className="text-xl py-5  font-serif ">{item.title}</h2>
+                <p className="text-lg cursor-pointer"> {item.pid}</p>
+                <p className="text-lg py-5 hover:text-xl cursor-pointer">₹ {item.price}</p>
+              </div>
+            </div>
+          </a>;
+        })}
+      </div>
+      <div>
 
-      <p>Hello Mom</p>
-
-      <div>Footer</div>
+      </div>
     </>
   );
 }
