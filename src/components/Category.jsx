35d40e1b0bf3a8eb06f9dff2a1b28226f90b3fa3
Sharun Ashwanth K V <sharunashwanth03@gmail.com>
2024-08-30 19:25:06 +0530

    review + recommendations

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -7,7 +7,7 @@ export default function Category(props) {
 
   return (
     <>
-      <section className="px-4 py-2 border-neutral-500">
+      <section className="max-w-full px-4 py-2 border-neutral-500">
         <h2 className="text-xl font-semibold mb-6">{categoryName}</h2>
         <div className="flex space-x-5 overflow-x-scroll no-scroll">
           {categoryData.map((item) => (
--- src/components/Items.jsx
+++ src/components/Items.jsx
@@ -1,6 +1,21 @@
 import { useFetch } from "@/utils/awesomeHooks";
 import React from "react";
 import Chatbot from "./Chatbot";
+import Loading from "./Loading";
+import Error from "./Error";
+import Category from "./Category";
+
+const getAvgRating = (ratings) => {
+    let one = 0;
+    let _one = 0;
+
+    for(let rating of ratings) {
+        if (rating === 1) one++;
+        if (rating === -1) _one++;
+    }
+
+    return parseInt( (one / (one + _one)) * 5 );    
+};
 
 function ProductDetails(props) {
     let details = props.details;
@@ -21,16 +36,25 @@ function ProductDetails(props) {
     </>;
 }
 
-function Review(props) {
-    let category = props.category;
-    let url = `https://acpproject021.pythonanywhere.com/api/reviews/?query=${category}`
-    const [data, error, loading] = useFetch(url);
-    
-    if ((loading === true) || (error !== null)) return <p>No review found</p>;
-    
-    return <>
-        <p>{url}</p>
+function Review({ reviewData }) {
+    let markup = reviewData.documents.map((review) => <>
+        <p>{review.review}</p>
     </>
+    );
+    
+    return <div>{markup}</div>
+}
+
+function Recommendations({ subCategory }) {
+  const [recommendationData, recommendationError, recommendationLoading] = useFetch(
+    `https://acpproject021.pythonanywhere.com/api/search/?query=${subCategory}`
+  );
+
+  if (recommendationLoading === true) return <Loading />;
+  if (recommendationError !== null) return <Error message={recommendationError} />;
+
+  console.log(recommendationData);
+  return <Category categoryName="Similar Products" categoryData={recommendationData.results} />
 }
 
 export default function Items(props) {
@@ -38,6 +62,13 @@ export default function Items(props) {
     let TrackItemPageSection = props.TrackItemPageSection;
     let setSectionTimingData = props.setSectionTimingData;
     let setAction = props.setAction;
+
+    const [reviewData, reviewError, reviewLoading] = useFetch(
+      `https://acpproject021.pythonanywhere.com/api/reviews/?query=${data.category}`
+    );
+    
+    if (reviewLoading === true) return <Loading />;
+    if (reviewError !== null) return <Error message={reviewError} />;
     
     return <>
         <div className="flex space-x-3 h-[90vh] overflow-hidden">
@@ -49,12 +80,15 @@ export default function Items(props) {
                 />
             </div>
 
-            <div className="w-auto bg-slate-300 overflow-scroll no-scroll">
+            <div className="w-[56%] bg-slate-300 overflow-scroll no-scroll">
                 <TrackItemPageSection id="item-detail" setSectionTimingData={setSectionTimingData}>
                     <div className="flex flex-col justify-start w-full">
                         <h2 className="text-xl font-semibold mb-2">{data.title}</h2>
                         <p className="text-lg font-bold text-gray-700 mb-2">{data.price} Rs</p>
+                        <p className="text-sm text-gray-500 mb-4">{data.brand}</p>
                         <p className="text-sm text-gray-500 mb-4">{data.category}</p>
+                        <p className="text-sm text-gray-500 mb-4">{data.sub_category}</p>
+                        <p className="text-sm text-gray-500 mb-4">Rating: {getAvgRating(reviewData.ratings)}</p>
                     </div>
                 </TrackItemPageSection>
                 
@@ -68,7 +102,12 @@ export default function Items(props) {
                 </TrackItemPageSection>
 
                 <TrackItemPageSection id="reviews" setSectionTimingData={setSectionTimingData}>
-                    <Review category={data.category}/>
+                    <h2>Reviews</h2>
+                    <Review reviewData={reviewData}/>
+                </TrackItemPageSection>
+
+                <TrackItemPageSection id="reviews" setSectionTimingData={setSectionTimingData}>
+                    <Recommendations subCategory={data.sub_category} />
                 </TrackItemPageSection>
 
                 <div className="fixed bottom-0">
@@ -79,7 +118,7 @@ export default function Items(props) {
             </div>
             
             <div className="w-2/6 h-[200vh] overflow-scroll bg-slate-300 no-scroll">
-                <Chatbot productData={data} />
+                <Chatbot productData={data} ratings={reviewData.ratings} />
             </div>
         </div>
 
