6115125c6ffd3abe06d2975ee0620bb48a0df9fa
Arun kumar D <arunmass0273@gmail.com>
2024-08-30 23:45:40 +0530

    item page declaration

--- src/components/ItemVertical.jsx
+++ src/components/ItemVertical.jsx
@@ -10,11 +10,11 @@ export default function ItemVertical(props) {
         key={itemData.pid}
         className="inline h-[320px] min-w-[260px] max-w-[260px] p-4 border-l-indigo-200 rounded-xl ring-1  ring-inset ring-slate-300 shadow-lg hover:shadow-slate-400 hover:scale-[1.015] hover:duration-75 overflow-hidden whitespace-nowrap text-ellipsis text-center"
       >
-        <a href={itemURL} className="w-full h-full flex flex-col items-center justify-between">
+        <a href={itemURL} className="w-full h-full flex flex-col items-center  justify-between">
         <img
             src={itemData.images[0]}
             alt={itemData.title}
-            className="min-h-[70%] rounded-md"
+            className="min-h-[70%] hover:scale-110 duration-150 rounded-md"
         />
         <div className="w-full flex flex-col justify-center items-center space-y-2">
             <p title={itemData.title} className="w-[80%] overflow-hidden whitespace-nowrap text-ellipsis">{itemData.title}</p>
--- src/components/Items.jsx
+++ src/components/Items.jsx
@@ -21,14 +21,14 @@ function ProductDetails(props) {
     let details = props.details;
     
     return <>
-        <table>
-            <tbody>
+        <table className="flex justify-around w-full text-center  "> 
+            <tbody className="border-b dark:border-slate-400">
                 {details.map((specs) => {
                     let spec = Object.entries(specs)[0];
 
-                    return <tr key={spec[0]}>
-                        <th>{spec[0]}</th>
-                        <td>{spec[1]}</td>
+                    return <tr className="border-b border-spacing-2 text-left space-x-60 space-y-1 border-s-slate-100 " key={spec[0]}>
+                        <th className="">{spec[0]}</th>
+                        <td className="float-start  border-b-neutral-50  dark:border-slate-50  py-4">{spec[1]}</td>
                     </tr>
                 })}
             </tbody>
@@ -37,8 +37,9 @@ function ProductDetails(props) {
 }
 
 function Review({ reviewData }) {
-    let markup = reviewData.documents.map((review) => <>
-        <p>{review.review}</p>
+    let markup = reviewData.documents.map((review, i) => <>
+        <h2 className="text-xl px-1  font-mono font-extrabold py-2  flex "><img src="/OIP.jpeg" alt="logo" className="w-12 rounded-full px-2" /> User - {i + 1}</h2>
+        <p className="text-lg px-10  py-1 mb-5">{review.review}</p>
     </>
     );
     
@@ -80,31 +81,30 @@ export default function Items(props) {
                 />
             </div>
 
-            <div className="w-[56%] bg-slate-300 ">
-                <div className="h-[95%] overflow-scroll no-scroll">
+            <div className="w-[45%] bg-slate-100 shadow-inner rounded-sm px-6 py-8">
+                <div className="h-[95%] overflow-scroll space-y-4 no-scroll">
                     <TrackItemPageSection id="item-detail" setSectionTimingData={setSectionTimingData}>
                         <div className="flex flex-col justify-start w-full">
-                            <h2 className="text-xl font-semibold mb-2">{data.title}</h2>
-                            <p className="text-lg font-bold text-gray-700 mb-2">{data.price} Rs</p>
-                            <p className="text-sm text-gray-500 mb-4">{data.brand}</p>
-                            <p className="text-sm text-gray-500 mb-4">{data.category}</p>
-                            <p className="text-sm text-gray-500 mb-4">{data.sub_category}</p>
-                            <p className="text-sm text-gray-500 mb-4">Rating: {getAvgRating(reviewData.ratings)}</p>
+                            <h2 className="text-2xl font-semibold px-2    py-4 mb-2">{data.title}</h2>
+                            <p className="text-2xl  font-bold px-4 py-2 text-gray-700 mb-2">₹{data.price} </p>
+                            <p className="text-base px-3 font-bold text-gray-500 mb-4">Brand:{data.brand}</p>
+                            <p className="text-base px-3 font-bold text-gray-500 mb-4">Category:{data.category}</p>
+                            <p className="text-base px-3 font-bold text-gray-500 mb-4">Sub_Category:{data.sub_category}</p>
+                            <p className="text-base px-3 font-bold text-gray-500 mb-4">Rating: {getAvgRating(reviewData.ratings)}</p>
                         </div>
                     </TrackItemPageSection>
                     
                     <TrackItemPageSection id="description" setSectionTimingData={setSectionTimingData}>
-                        <p className="text-sm text-gray-600 mb-4">{data.description}</p>
+                        <p className="text-lg px-1 font-extrabold   mb-9">{data.description}</p>
                     </TrackItemPageSection>
                     <TrackItemPageSection id="specifications" setSectionTimingData={setSectionTimingData}>
-                        <h3 className="text-md font-medium mb-2">Specifications</h3>
+                        <h3 className="text-lg font-extrabold px-2 py-5 font-medium mb-2">Specifications</h3>
                         <ProductDetails details={data.product_details} />
                     </TrackItemPageSection>
                     <TrackItemPageSection id="reviews" setSectionTimingData={setSectionTimingData}>
-                        <h2>Reviews</h2>
                         <Review reviewData={reviewData}/>
                     </TrackItemPageSection>
-                    <TrackItemPageSection id="reviews" setSectionTimingData={setSectionTimingData}>
+                    <TrackItemPageSection id="recommendations" setSectionTimingData={setSectionTimingData}>
                         <Recommendations subCategory={data.sub_category} />
                     </TrackItemPageSection>
                 </div>
